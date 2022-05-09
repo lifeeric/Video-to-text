8d29af4d4929082e102a0376777f6b0991ff262b
EricGit <hi@ericgit.me>
2022-05-09 02:32:00 -0400

    [FIXED] now deletes video file after creating transcript. add logs

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 import sys
 import os
-import wave, math, contextlib
+import wave
+import math
+import contextlib
 import speech_recognition as sr
 from moviepy.editor import AudioFileClip
 
-video_file_name = sys.argv[1];
+print('[SUCCESS] started processing the file')
+
+video_file_name = sys.argv[1]
 zoom_video_file_name = video_file_name
 
 file_name = zoom_video_file_name.split('.')
 transcribed_audio_file_name = f"{file_name[0]}.wav"
 
 if not os.path.exists(video_file_name):
-    print("[ERROR] Video file doesn't exists");
+    print("[ERROR] Video file doesn't exists")
+    print("""
+        /////////////////////////////////////////////
+        please report your error: https://ericgit.me
+        /////////////////////////////////////////////
+
+    """)
     sys.exit('[Bye]')
 
 audioclip = AudioFileClip(zoom_video_file_name)
 audioclip.write_audiofile(transcribed_audio_file_name)
 
-print(f'[CREATING] transcript to {file_name[0]}-trans.txt');
+print(f'[SUCCESS] creating transcript to {file_name[0]}-trans.txt')
 
-with contextlib.closing(wave.open(transcribed_audio_file_name,'r')) as f:
+with contextlib.closing(wave.open(transcribed_audio_file_name, 'r')) as f:
     frames = f.getnframes()
     rate = f.getframerate()
     duration = frames / float(rate)
@@ -32,6 +42,18 @@ for i in range(0, total_duration):
     f = open(f"{file_name[0]}-trans.txt", "a")
     f.write(r.recognize_google(audio))
     f.write(" ")
+
+print('[SUCCESS] Deleting video file')
+os.remove(transcribed_audio_file_name)
+os.remove(zoom_video_file_name)
+
 f.close()
 
-print('[DONE')
+print("""
+//////////////////////////////////////////////////////////////////
+                            DONE
+//////////////////////////////////////////////////////////////////
+            please report your error: https://ericgit.me
+""")
+print('')
+print('')
